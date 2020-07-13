8d433f76fb91dd41ae4ceafc330825feea1c9875
niruos <66792132+niruos@users.noreply.github.com>
2020-07-13 22:57:29 +0300

    Add files via upload

---
+++ js/translater.js
@@ -0,0 +1,235 @@
+var new_lang = {
+
+    "lt0": "Publikácie:",
+    
+    "lt1": "US",
+    
+    "lt2": "UK",
+    
+    "lt3": "España",
+    
+    "lt4": "Italia",
+    
+    "lt5": "Nederland",
+    
+    "lt6": "台灣",
+    
+    "lt7": "Japan",
+    
+    "lt8": "Štýl",
+    
+    "lt9": "Krása",
+    
+    "lt10": "Sex",
+    
+    "lt11": "Politika",
+    
+    "lt12": "Astrológia",
+    
+    "lt13": "Predplatné",
+    
+    "lt14": "Darovať",
+    
+    "lt15": "Elektronické vydania",
+    
+    "lt16": "Newsletter",
+    
+    "lt17": "Slovensko",
+    
+    "lt18": "Slovensko",
+    
+    "lt19": "Vyhľadávanie",
+    
+    "lt20": "Nemôžete neschudnúť!",
+    
+    "lt21": "Známy odborník na výživu prehovoril o novom trende chudnutia - ketónovej strave, ktorá spaľuje vnútorný tuk na plne automatickom stroji",
+    
+    "lt22": "By<a class=\"byline-name\" data-vars-ga-call-to-action=\"Anna Jalová\" data-vars-ga-ux-element=\"Byline\" href=\"\"><span class=\"byline-name\">Anna Jalová</span></a>",
+    
+    "lt23": "21. októbra 2019",
+    
+    "lt24": "Nadváha je nebezpečná pre ľudské zdravie a osobný život. Lekári to neúnavne opakujú. Avšak tí, ktorí trpia nadváhou, vedia, ako ťažké je sa zbaviť nadmernej váhy. Najmä ak nemáte pár kilogramov navyše, ale desať alebo viac.",
+    
+    "lt25": "Avšak dnes aj v tomto prípade MÔŽETE SCHUDNÚŤ. A nielen to - teraz je to opravdu ľahké urobiť! V roku 2016 vedci urobili prielom v dietetike - dozvedeli sa, čo presne v tele spôsobuje to, že tuk sa premieňa na svaly a energiu a naučili sa, ako tento proces ovplyvniť. Pri použití novej metódy je možné rýchlo schudnúť, takže boj proti podkožnému tuku sa uskutočňuje na úrovni biochemických procesov.",
+    
+    "lt26": "Povedať viac o novej metóde chudnutia, konkrétne o ketónovej diéte, požiadali sme slávneho odborníka na výživu, lekára najvyššej kategórie, profesora Ľuboša Záhorského",
+    
+    "lt27": "Ľuboš Záhorský",
+    
+    "lt28": "- Aký je význam keto-diéty a kvôli čomu nám umožňuje vysporiadať sa s kilami navyše?",
+    
+    "lt29": "- No pozrite sa. Podstatou ketónovej stravy je obnoviť telo na lipolýzu (premenu tukov na energiu) z glykolýzu (rozklad cukrov). Naše telo je veľmi flexibilné a má obrovské príležitosti na prispôsobenie sa ku všetkému. Keď prestaneme jesť sacharidy, telo začne používať uložené tuky.",
+    
+    "lt30": "Ako to ide? V pečeni s predĺženou neprítomnosťou sacharidov v strave (2 až 3 týždne) sa začínajú vyrábať ketónové telá. Cestujú telom spolu s krvou a spôsobujú, že tuky sa menia na energiu, ktorú potrebujeme.",
+    
+    "lt31": "Vedci zistili tento mechanizmus spracovania uložených tukov relatívne nedávno. A dnes sa tento spôsob považuje za najsľubnejší v boji proti kilogramom. Podľa štatistík s pomocou ketózy chudne 99 ľudí zo 100 bez ohľadu na dôvody priberania na váhe, veku, pohlaví alebo osobných preferenciách v potrave. Chudnutie sa uskutočňuje na úrovni biochémie.",
+    
+    "lt32": "Ketónová strava je omnoho účinnejšia ako väčšina známych diét. Napríklad diéta Ducana alebo Atkinsa. Okrem toho nielenže nepoškodzuje organizmus, ako ho poškodzujú tieto diéty, ale tiež ho uzdravuje. Toto je skutočný prielom v dietetike.",
+    
+    "lt33": "- Práve ste povedali, že pomocou ketónovej diéty môže človek automaticky schudnúť, bez ohľadu na to, aké má v strave závislosti. Zatiaľ čo predtým ste povedali, že na spustenie ketózy by sa z diéty mali vylúčiť sacharidy. Ako to funguje?",
+    
+    "lt34": "- V skutočnosti nejde o žiadný rozpor. Skutočnosť je taká, že samotná ketónová strava je pre organizmus veľmi zložitá a naše telo ju ťažko toleruje. To znamená, že úplné vylúčenie sacharidov na vyvolanie ketózy v tele je veľmi závažným testom na organizmus, ktorý sotva vôbec niekto môže prejsť. Iba ak sa ocitne na púštnom ostrove, kde nebude žiadne jedlo. V bežnom našom živote je to nemožné.",
+    
+    "lt35": "Vedci si to však pomysleli. Faktom je, že ketónové telá nie sú produkované iba ľudskou pečeňou. Tieto látky alebo skôr ich analógy pôsobia aj v niektorých rastlinách. V roku 2016 urobili americkí odborníci na výživu dôležitý objav - dostali ketónové telá z malín! To znamená, a to chcem znova zopakovať, že pri chudnutí pomocou ketózy nemusíte vôbec zanechávať sacharidy alebo meniť svoju obvyklú stravu.",
+    
+    "lt36": "Chudnutie s ketózou je dobré, pretože kombinuje vysokú účinnosť a jednoduchosť. Všetko, čo človek potrebuje, je pridať do bežnej stravy ketónové telá. V skutočnosti to znamená denne užívať špeciálnu kapsulu obsahujúcu tieto ketóny. Akonáhle tieto ketóny budú v krvi, budú vykonávať rovnakú funkciu ako ketónové telá produkované v pečeni človeka. Konkrétne, odbúravať telesný tuk.",
+    
+    "lt37": "Čím dlhšie človek užíva tieto ketóny, tým viac schudne. Pretože ketóny automaticky spracúvajú tuk. Predstavte si napríklad tuk, ktorý sa nahromadil na tanieri. A do tohto tuku ste pridali prostriedok na riad.  Tuk sa začal rozpúšťať. Toto je, samozrejme, veľmi hrubé porovnanie. Ale to isté sa deje na úrovni organizmu. A to sa nevyhnutne.",
+    
+    "lt38": "stáva s naším tukom v automatickom režime. Človek necíti žiadne negatívne okamihy, nemusí športovať, nemusí hladovať. Ak si zvyknete ráno vážiť, potom pri užívaní ketónov si každý deň všimnete zníženie hmotnosti.",
+    
+    "lt39": "Chcem vám ukázať fotky mojich klientov, ktorí schudli pomocou ketózy. Toto sú najťažšie prípady - bez ketónov by títo ľudia nemohli skôr vôbec schudnúť.",
+    
+    "lt40": "Eliza, pred - 107 kg, po - 54 kg",
+    
+    "lt41": "Eva, pred - 120 kg, po - 65 kg",
+    
+    "lt42": "Maria pred - 105 kg, po - 55 kg",
+    
+    "lt43": "A chcem ešte ráz dodať, že táto metóda je nielen úplne bezpečná, ale aj užitočná pre celé telo. Ľudia sa stávajú viac zdravými po chudnutí.",
+    
+    "lt44": "- Ako brať ketónové telieska? Aký výrobok ich obsahujú?",
+    
+    "lt45": "- Doposiaľ existuje iba jeden komplex, ktorý obsahuje rastlinné ketónové telá. A to je KETO DIET. Dnes tento komplex používa 80% odborníkov na výživu, pretože poskytuje rýchle a ľahké chudnutie. Vo väčšine prípadov odporúčajú ho svojim zákazníkom.",
+    
+    "lt46": "KETO DIET",
+    
+    "lt47": "Okrem ketónových telies KETO DIET obsahuje asi 10 ďalších komponentov potrebných na rýchle a ľahké chudnutie. Zameriavajú sa na zlepšenie pôsobenia ketónov, ako aj na podporu tela pri chudnutí. Nebudem ich všetky uvádzať, uvediem iba niekoľko.",
+    
+    "lt48": "Kommifora Mukul",
+    
+    "lt49": "To je nikátna zložka, ktorá zrýchľuje metabolizmus telesného tuku o 7-10 krát. Pomáha ketónom vyrovnať sa s najzložitejším tukom.",
+    
+    "lt50": "L- karnitín",
+    
+    "lt51": "Zvyšuje oxidáciu mastných kyselín, podporuje prenos tuku do svalov a zabraňuje hromadeniu nového tuku.",
+    
+    "lt52": "Bromelain",
+    
+    "lt53": "Vďaka svojim vlastnostiam zlepšuje fungovanie pankreasu, zmierňuje opuchy a zabraňuje obezite vnútorných orgánov.",
+    
+    "lt54": "Beta sitosterol",
+    
+    "lt55": "Neutralizuje účinky podvýživy, odstraňuje toxíny z tela a zvyšuje elasticitu pokožky, zabraňuje vzniku strií.",
+    
+    "lt56": "Koenzým Q 10",
+    
+    "lt57": "Aktívne odbúrava viscerálny tuk, znižuje «zlý» cholesterol, potláča chuť do jedla a eliminuje prejedanie.",
+    
+    "lt59": "Chcem vám ukázať výsledky testov tohto komplexu na Slovenskom ústavu diabetologie a správnej výživy. Celkovo sa testov zúčastnilo asi 1000 dobrovoľníkov trpiacich obezitou. Všetci užívali KETO DIET mesiac.",
+    
+    "lt60": "Výsledky testu: ",
+    
+    "lt61": "Strata hmotnosti o 10 kg a viac - 18% testovaných",
+    
+    "lt62": "Strata hmotnosti o 5-10 kg - 77% testovaných",
+    
+    "lt63": "Strata hmotnosti menej ako 5 kg - 4% testovaných",
+    
+    "lt64": "Nestratili žiadnu hmotnosť - 1% testovaných",
+    
+    "lt65": "Zlepšenie stavu pokožky, boj proti celulitíde - 98% testovaných",
+    
+    "lt66": "Normalizácia biochemických procesov v krvi - 100% testovaných ",
+    
+    "lt67": "Žiadne vedľajšie účinky a závislosť - 100% testovaných",
+    
+    "lt68": "Ako vidíte, KETO DIET pomáha takmer všetkým. Každý, kto ho začne brať, ZARUČOVANÉ stráca váhu. Komplex je možné užívať s veľkou hmotnosťou (pri nadváhe ohrozujúcej zdravie), ale aj s niekoľkými kilogramami navyše, keď napríklad chcete schudnúť na dovolenke.",
+    
+    "lt69": "- Kde môžem získať KETO DIET a koľko to stojí, aspoň približne?",
+    
+    "lt70": "- Najjednoduchší spôsob je objednať si KETO DIET na <a href=\"\">oficiálnych webových stránkach</a> kuriérom alebo poštou kdekoľvek na Slovensku.",
+    
+    "lt71": "Ako som už povedal, dnes je chudnutie s ketónovou diétou mimoriadne populárne, a preto existuje veľa falzifikátov KETO DIET, ktoré neprinášajú žiadne výsledky, preto odporúčam objednať KETO DIET iba na oficiálnych webových stránkach výrobcu! Na tejto stránke sa predávajú iba originálne výrobky.",
+    
+    "lt72": "Mimochodom, teraz je na webe super akcia, takže je možné KETO DIET kúpiť so 50% zľavou! Chcem len upozorniť, pokiaľ viem, že výrobca identifikoval iba malú dávku akčného výrobku, ktorá rýchlo končí. Preto, ak si chcete objednať KETO DIET oveľa lacnejšie ako jeho obyčajná cena (a samotný výrobok je dosť drahý), musíte čo najskôr zanechať žiadosť, zatiaľ čo akčné balíčky sú stále k dispozícii.",
+    
+    "lt73": "Oficiálna webová stránka KETO DIET na Slovensku",
+    
+    "lt74": "Komentáre",
+    
+    "lt75": "Tento komplex je naozaj skvelý. Mám dobré skúsenosti s jeho užívaním. Schudla som rýchlo<br><b> Petra</b> ",
+    
+    "lt76": "Ďakujem! Znie to veľmi zaujímavé. Objednala som si tento komplex. Začnem chudnúť. Už veľa som počula o keto strave, ale nevedela som, že ketóny sa môžu užívať samostatne!<br><b> Alica K.</b> ",
+    
+    "lt77": "Výrobok je super!!! Úplne potvrdzujem ho účinnosť! Nemohla som schudnúť skoro 10 rokov. Počas tejto doby som vyskúšala asi všetko! Nič mi nepomohlo. Len čo som začala užívať KETO DIET, hmotnosť za pár týždňov zmizla. A už som znovu štíhla!<br><b> Eva</b> ",
+    
+    "lt78": "Výborne. Objednávam, zatiaľ čo stíham kúpiť tento komplex so zľavou.<br><b>Lenka</b> ",
+    
+    "lt79": "O tomto lieku hovorila mi kamarátka. Za pár mesiacov schudla o 21 kg. Nechodila športovať, ani nemala žiadny hlad. Teraz sa cíti skvele. Okamžite sa objavilo veľa sukničkárov.<br><b> Maria V.</b> ",
+    
+    "lt80": "Stratila som 20 kg. Okrem toho som tiež chodila do fitka, pretože naozaj nevedela som kam dať energiu. Stala som sa zreteľne štíhlejšie. Ak sa predtým chlapci ani nepozerali mojím smerom, dnes každý deň počujem komplimenty. To je také príjemné. To sú moje fotky<img alt=\"\" src=\"12.jpg\" style=\"width: 100%; display: block; margin: 5px auto;\"><b> Ján</b> ",
+    
+    "lt81": "Tiež mám skúsenosti s užívaním KETO DIET a tiež sú pozitívne. Vážil som 98 kg, teraz 81 kg. Úplne som spokojný.<br><b> Tomáš Turenič</b> ",
+    
+    "lt82": "Podrobnosti o KETO DIET som si prečítala na tejto webovej stránke. Nakoniec vedci prišli niečím novým a opravdu vynikajúcim! <br><b> Monika</b> ",
+    
+    "lt83": "To je skutočne nejaký zázrak! Skutočne som neverila, že mi tento komplex pomôže. Pretože ostatní lieky mi nepomohli, ale 3 kila už preč! Používam KETO DIET iba 4 dni. Nemenila som svoju stravu<br><b>  Maria R.</b> ",
+    
+    "lt84": "Tiež chcem taký komplex! A už urobila som objednávku, zatiaľ čo ešte máju tento zázračný výrobok na sklade.<br><b> Petra Savanová</b> ",
+    
+    "lt85": "S manželom sme schudli spolu. Keď sa komplex práve objavil na trhu, odporučil nám ho známy odborník na výživu. Teraz sme obaja tenkí, spolu sme sa zbavili 41 kg, teraz vedieme zdravý životný štýl. Byť štíhly - je to úžasné!<br><b> Veronika</b> ",
+    
+    "lt86": "Všetky moji kamarátky hovoria iba o KETO DIET. S jeho pomocou veľa z nich schudla o niekoľko kíl. Toto je skutočný hit medzi mojimi kamarátkami <br><b> Ingrida V.</b> ",
+    
+    "lt87": "Ďakujem.  Toto je dobrá správa<br><b> Katka Lukášová</b> ",
+    
+    "lt88": "Tiež som schudla pomocou KETO DIET!!! 18 kg za 7 týždňov. Predtým som nikdy nemohla schudnúť o toľko kíl. Veľmi pekné ďakujem za tento článok!!! <br><b> Eva</b> ",
+    
+    "lt89": "Objednala som. Podľa tejto akcie stále sa dá kúpiť výrobok, ale zostáva ho naozaj veľmi málo.<br><b> Lenka K.</b> ",
+    
+    "lt90": "Oficiálna webová stránka KETO DIET na Slovensku",
+    
+    "lt91": "Zdieľajte to",
+    
+    "lt92": "O nás",
+    
+    "lt93": "Instagram",
+    
+    "lt94": "Reklama",
+    
+    "lt95": "Media",
+    
+    "lt96": "Informácie pre klientov",
+    
+    "lt97": "Voľné pracovné miesta v Cosmo",
+    
+    "lt98": "YouTube",
+    
+    "lt99": "Predplatné",
+    
+    "lt100": "Cosmo obchod",
+    
+    "lt101": "Elektronické vydania",
+    
+    "lt102": "Newsletter",
+    
+    "lt103": "Darčeky",
+    
+    "lt104": "Súčasť digitálnej edície Hearst",
+    
+    "lt105": "Cosmopolitan sa podieľa na rôznych programoch pridruženého marketingu.",
+    
+    "lt106": "©2019 Hearst Magazine Media, Inc. Všetky práva vyhradené.",
+    
+    "lt107": "Zásady ochrany osobných údajov",
+    
+    "lt108": "Cielená reklama",
+    
+    "lt109": "Pravidla",
+    
+    "lt110": "Mapa stránky",
+    
+    "lt111": " "
+    
+};
+function Translater () {
+ for (class_name in new_lang) {
+ var elements = document.getElementsByClassName(class_name);
+ if (elements.length) {
+ for (key in elements) {
+ elements[key].innerHTML = new_lang[class_name];
+ }
+ }
+ }
+ };
---
+++ zi/assets/dist/js/shared/precache-manifest.84da09c189d0fefb70fd214df5e05be5.js
@@ -0,0 +1,10 @@
+self.__precacheManifest = [
+  {
+    "revision": "f769c532396f75487da9",
+    "url": "../https@assets.hearstapps.com/assets/dist/js/shared/vendors.7481b51fff9811923eca.js"
+  },
+  {
+    "revision": "5fa4da6ff833d4e8c9d8",
+    "url": "../https@assets.hearstapps.com/assets/dist/js/shared/jquery.cc91ea4d905d80d26bce.js"
+  }
+];
